92870fd0711f63069b2538868acde9e0ea5db5a1
Code Explore <codeandexplore@gmail.com>
2020-02-09 17:07:12 +0800

    Initialize AspNet Identity DbContext and Migration

---
+++ src/Codx.Auth/Data/Contexts/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Codx.Auth.Data.Entities.AspNet;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codx.Auth.Data.Contexts
+{
+    public class UserDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
