ddaaeb763a0f9071a4fb2024ab84868cfc9c00e3
Code Explore <codeandexplore@gmail.com>
2021-04-03 10:15:38 +0800

    Add Clients

---
+++ src/Codx.Auth/Controllers/ClientsController.cs
@@ -0,0 +1,71 @@
+﻿using Codx.Auth.ViewModels;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codx.Auth.Controllers
+{
+    public class ClientsController : Controller
+    {
+        protected readonly ConfigurationDbContext _dbContext;
+        public ClientsController(ConfigurationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var clients = _dbContext.Clients.ToList();
+
+            var viewModel = clients.Select(client => new ClientDetailsViewModel
+            {
+                Id = client.Id,
+                ClientId = client.ClientId,
+                ClientName = client.ClientName,
+                Description = client.Description,
+            }).ToList();
+
+            return View(viewModel);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ClientAddViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var record = new Client
+                {
+                    ClientId = viewModel.ClientId,
+                    ClientName = viewModel.ClientName,
+                    Description = viewModel.Description,
+                    Enabled = true,
+                    Created = DateTime.UtcNow,
+                };
+
+                await _dbContext.AddAsync(record).ConfigureAwait(false);
+                var result = await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+
+                ModelState.AddModelError("", "Failed");
+
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ src/Codx.Auth/ViewModels/ClientViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codx.Auth.ViewModels
+{
+    public class ClientViewModels
+    {
+    }
+
+    public class ClientDetailsViewModel
+    {  
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ClientAddViewModel : BaseClientViewModel
+    {
+
+    }
+
+    public class ClientEditViewModel : BaseClientViewModel
+    {
+
+    }
+
+    public class BaseClientViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string ClientId { get; set; }
+        [StringLength(200)]
+        public string ClientName { get; set; }
+        [StringLength(1000)]
+        public string Description { get; set; }
+    }
+}
