76d5adf9e6123dd9a6fa69c2cf2e685068fea8b4
Code Explore <codeandexplore@gmail.com>
2021-04-03 10:45:35 +0800

    Add Client Claims

---
+++ src/Codx.Auth/Controllers/ClientClaimsController.cs
@@ -0,0 +1,88 @@
+﻿using Codx.Auth.ViewModels;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codx.Auth.Controllers
+{
+    public class ClientClaimsController : Controller
+    {
+        protected readonly ConfigurationDbContext _dbContext;
+        public ClientClaimsController(ConfigurationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> ClientClaims(int id)
+        {
+            var client = await _dbContext.Clients.Include(i => i.Claims).FirstOrDefaultAsync(u => u.Id == id);
+
+            var viewmodel = new ClientClaimsDetailsViewModel();
+
+            viewmodel.ClientId = client.Id;
+            viewmodel.ClientIdString = client.ClientId;
+            viewmodel.ClientName = client.ClientName;
+            viewmodel.Description = client.Description;
+
+            foreach (var claim in client.Claims)
+            {
+                viewmodel.Claims.Add(new ClientClaimDetailsViewModel
+                {
+                    ClaimId = claim.Id,
+                    ClaimType = claim.Type,
+                    ClaimValue = claim.Value,
+                });
+            }
+
+            return View(viewmodel);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Add(int clientid)
+        {
+            var client = await _dbContext.Clients.Include(i => i.Claims).FirstOrDefaultAsync(u => u.Id == clientid);
+
+            var viewmodel = new ClientClaimAddViewModel();
+
+            viewmodel.ClientId = client.Id;
+            viewmodel.ClientIdString = client.ClientId;
+            viewmodel.ClientName = client.ClientName;
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ClientClaimAddViewModel viewmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = await _dbContext.Clients.Include(i => i.Claims).FirstOrDefaultAsync(u => u.Id == viewmodel.ClientId);
+
+                client.Claims.Add(new ClientClaim
+                {
+                    ClientId = viewmodel.ClientId,
+                    Type = viewmodel.ClaimType,
+                    Value = viewmodel.ClaimValue
+                });
+
+                var result = await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(ClientClaims), new { id = viewmodel.ClientId });
+                }
+
+                ModelState.AddModelError("", "Failed");
+            }
+
+            return View(viewmodel);
+        }
+
+    }
+}
---
+++ src/Codx.Auth/ViewModels/ClientClaimViewModels.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codx.Auth.ViewModels
+{
+    public class ClientClaimViewModels
+    {
+    }
+
+    public class ClientClaimDetailsViewModel 
+    {
+        public int Id { get; set; }
+
+        public int ClaimId { get; set; }
+
+        public string ClaimType { get; set; }
+
+        public string ClaimValue { get; set; }
+    }
+
+    public class ClientClaimsDetailsViewModel
+    {
+        public ClientClaimsDetailsViewModel()
+        {
+            Claims = new List<ClientClaimDetailsViewModel>();
+        }
+
+        public int ClientId { get; set; }
+        public string ClientIdString { get; set; }
+        public string ClientName { get; set; }
+        public string Description { get; set; }
+        public List<ClientClaimDetailsViewModel> Claims { get; set; }
+    }
+
+
+    public class ClientClaimAddViewModel : BaseClientClaimViewModel
+    {
+        public string ClientIdString { get; set; }
+        public string ClientName { get; set; }
+    }
+
+    public class ClientClaimEditViewModel : BaseClientClaimViewModel
+    {
+        public string ClientIdString { get; set; }
+        public string ClientName { get; set; }
+    }
+
+    public class BaseClientClaimViewModel
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string ClaimType { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string ClaimValue { get; set; }
+    }
+
+
+}
