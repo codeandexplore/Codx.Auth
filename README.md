80230ceaad860269510874dc7e2a1e99e4190825
Diony Code <dionycode@outlook.com>
2022-07-24 07:37:33 +0800

    Add default users and roles

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
+
 # Codx.Auth
 Code and Explore Authentication and Authorization Project
+
+This is created using 
+- .NET Core 3.1
+- IdentityServer4 4.1.2
+- EntityFrameworkCore 5.0.4
+
+For the data access, we have three DbContext
+- UserDbContext - For the AspNet Identity
+- ConfigurationDbContext - For the IdentityServer4
+- PersistedGrantDbContext - For the IdentityServer4
+
+# To Use the Project
+
+1. Clone the project to your local machine
+2. Open the Codx.Auth solution using Visual Studio
+
+3. In the appsettings.json, add connection string
+Example:
+
+```
+
+ "ConnectionStrings": {
+    "DefaultConnection": "Server=DB_SERVER;Database=codx_auth_db;User Id=DB_USER;Password=DB_PASSWORD;"
+  },
+
+```
+- DB_SERVER - Your Database IP or DNS Name
+- DB_USER - Your Database Username
+- DB_PASSWORD - Your Database User Password
+
+3. Run the application
+There are two default users
+|   Type          | Username      | Password        |
+|-----------------|---------------|-----------------|
+|   Administrator | administrator | AdminPass12345! |
+|   User          | user          | UserPass12345!  |
+These values are seeded on application startup. You can change the values in **src/Codex.Auth/Extensions/DatabaseContext.cs**
---
+++ src/Codx.Auth/Extensions/DatabaseContext.cs
@@ -0,0 +1,159 @@
+﻿using Codx.Auth.Data.Contexts;
+using Codx.Auth.Data.Entities.AspNet;
+using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Codx.Auth.Extensions
+{
+    public static class DatabaseContext
+    {
+        public static IApplicationBuilder InitializeDb(this IApplicationBuilder app)
+        {
+
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                try
+                {
+                    var userDbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+                    var configurationDbContext = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                    var persistedGrantDbContext = scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>();
+
+                    userDbContext.Database.Migrate();
+                    configurationDbContext.Database.Migrate();
+                    persistedGrantDbContext.Database.Migrate();
+
+                    // Execute Seed if there is no user data found
+                    if (!userDbContext.Users.Any() && !userDbContext.Roles.Any())
+                    {
+                        SeedInitialAdminUser(userDbContext);
+                        SeedInitialUser(userDbContext);
+                    }                    
+
+                }
+                catch (Exception ex)
+                {
+                    // Add Logger here
+                }
+            }
+
+            return app;
+        }
+
+        private static void SeedInitialAdminUser(UserDbContext userDbContext)
+        {
+            var defaultUserId = Guid.NewGuid();
+            var defaultUsername = "administrator";
+            var defaultUserEmail = "administrator@app.co";
+            var defaultUserPassword = "AdminPass12345!";
+            var defaultRoleId = Guid.NewGuid();
+            var defaultRoleName = "Administrator";
+
+            if (!userDbContext.Users.Any(o => o.UserName == defaultUsername))
+            {
+                ApplicationUser defaultUser = new ApplicationUser
+                {
+                    Id = defaultUserId,
+                    UserName = defaultUsername,
+                    NormalizedUserName = defaultUsername.ToUpper(),
+                    Email = defaultUserEmail,
+                    NormalizedEmail = defaultUserEmail.ToUpper(),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+                defaultUser.PasswordHash = passwordHasher.HashPassword(defaultUser, defaultUserPassword);
+
+                userDbContext.Users.Add(defaultUser);
+                userDbContext.SaveChanges();
+            }
+
+
+            if (!userDbContext.Roles.Any(o => o.Name == defaultRoleName))
+            {
+                ApplicationRole defaultRole = new ApplicationRole
+                {
+                    Id = defaultRoleId,
+                    Name = defaultRoleName,
+                    NormalizedName = defaultRoleName.ToUpper()
+                };
+
+                userDbContext.Roles.Add(defaultRole);
+                userDbContext.SaveChanges();
+            }
+
+            if (!userDbContext.UserRoles.Any(o => o.UserId == defaultUserId && o.RoleId == defaultRoleId))
+            {
+                ApplicationUserRole defaultUserRole = new ApplicationUserRole
+                {
+                    UserId = defaultUserId,
+                    RoleId = defaultRoleId
+                };
+
+                userDbContext.UserRoles.Add(defaultUserRole);
+                userDbContext.SaveChanges();
+            }
+                        
+        }
+
+        private static void SeedInitialUser(UserDbContext userDbContext)
+        {
+            var defaultUserId = Guid.NewGuid();
+            var defaultUsername = "user";
+            var defaultUserEmail = "user@app.co";
+            var defaultUserPassword = "UserPass12345!";
+            var defaultRoleId = Guid.NewGuid();
+            var defaultRoleName = "User";
+
+            if (!userDbContext.Users.Any(o => o.UserName == defaultUsername))
+            {
+                ApplicationUser defaultUser = new ApplicationUser
+                {
+                    Id = defaultUserId,
+                    UserName = defaultUsername,
+                    NormalizedUserName = defaultUsername.ToUpper(),
+                    Email = defaultUserEmail,
+                    NormalizedEmail = defaultUserEmail.ToUpper(),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+                defaultUser.PasswordHash = passwordHasher.HashPassword(defaultUser, defaultUserPassword);
+
+                userDbContext.Users.Add(defaultUser);
+                userDbContext.SaveChanges();
+            }
+
+
+            if (!userDbContext.Roles.Any(o => o.Name == defaultRoleName))
+            {
+                ApplicationRole defaultRole = new ApplicationRole
+                {
+                    Id = defaultRoleId,
+                    Name = defaultRoleName,
+                    NormalizedName = defaultRoleName.ToUpper()
+                };
+
+                userDbContext.Roles.Add(defaultRole);
+                userDbContext.SaveChanges();
+            }
+
+            if (!userDbContext.UserRoles.Any(o => o.UserId == defaultUserId && o.RoleId == defaultRoleId))
+            {
+                ApplicationUserRole defaultUserRole = new ApplicationUserRole
+                {
+                    UserId = defaultUserId,
+                    RoleId = defaultRoleId
+                };
+
+                userDbContext.UserRoles.Add(defaultUserRole);
+                userDbContext.SaveChanges();
+            }
+
+        }
+    }
+}
