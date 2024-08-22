4c98c54e4f6ed4d62e39496733104963a18b9604
Diony S. Castro <dionycode@gmail.com>
2024-08-23 07:43:45 +0800

    Update to NET8.0 and Duende IdentityServer 7.0

--- src/Codx.Auth/Data/Migrations/IdentityServer4/ConfigurationDb/20240822232512_UpdateToDuende_v7.0.Designer.cs
+++ src/Codx.Auth/Data/Migrations/IdentityServer4/ConfigurationDb/20240822232512_UpdateToDuende_v7.0.Designer.cs
@@ -4,6 +4,7 @@ using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Codx.Auth.Data.Migrations.IdentityServer4.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240822232512_UpdateToDuende_v7.0")]
+    partial class UpdateToDuende_v70
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Codx.Auth/Data/Migrations/IdentityServer4/PersistedGrantDb/20240822232447_UpdateToDuende_v7.0.Designer.cs
+++ src/Codx.Auth/Data/Migrations/IdentityServer4/PersistedGrantDb/20240822232447_UpdateToDuende_v7.0.Designer.cs
@@ -4,6 +4,7 @@ using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Codx.Auth.Data.Migrations.IdentityServer4.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    partial class PersistedGrantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240822232447_UpdateToDuende_v7.0")]
+    partial class UpdateToDuende_v70
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
