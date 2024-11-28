263b15630633dbb68398fb6242530c0119892ba1
Diony S. Castro <dionycode@gmail.com>
2024-11-28 19:51:56 +0800

    Update my claims roles and companies

--- src/Codx.Auth/Controllers/MyProfileController.cs
+++ src/Codx.Auth/Controllers/MyProfileController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 
 namespace Codx.Auth.Controllers
@@ -41,6 +42,70 @@ namespace Codx.Auth.Controllers
             }           
 
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public JsonResult GetMyClaimsTableData(string search, string sort, string order, int offset, int limit)
+        {
+            var userId = User.GetUserId();
+            var query = _userdbcontext.UserClaims.Where(o => o.UserId == userId);
+
+            var data = query.OrderBy(o => o.Id).Skip(offset).Take(limit).ToList();
+            var viewModel = data.Select(claim => new UserClaimDetailsViewModel
+            {
+                Id = claim.Id,
+                ClaimType = claim.ClaimType,
+                ClaimValue = claim.ClaimValue,
+            }).ToList();
+
+            return Json(new
+            {
+                total = query.Count(),
+                rows = viewModel
+            });
+        }
+
+        [HttpGet]
+        public JsonResult GetMyRolesTableData(string search, string sort, string order, int offset, int limit)
+        {
+            var userId = User.GetUserId();
+            var query = _userdbcontext.UserRoles.Include(o => o.Role).Where(o => o.UserId == userId);
+
+            var data = query.Skip(offset).Take(limit).ToList();
+            var viewModel = data.Select(userrole => new UserRoleDetailsViewModel
+            {
+                RoleId = userrole.RoleId,
+                Role = userrole.Role.Name,
+            }).ToList();
+
+            return Json(new
+            {
+                total = query.Count(),
+                rows = viewModel
+            });
+        }
+
+        [HttpGet]
+        public JsonResult GetMyCompaniesTableData(string search, string sort, string order, int offset, int limit)
+        {
+            var userId = User.GetUserId();
+            var query = _userdbcontext.UserCompanies.Include(o => o.Company).ThenInclude(c => c.Tenant).Where(o => o.UserId == userId);
+
+            var data = query.OrderBy(o => o.Company.Name).Skip(offset).Take(limit).ToList();
+            var viewModel = data.Select(userCompany => new UserCompanyDetailsViewModel
+            {
+                UserId = userCompany.UserId,
+                CompanyId = userCompany.CompanyId,
+                CompanyName = userCompany.Company.Name,
+                TenantId = userCompany.Company.TenantId,
+                TenantName = userCompany.Company.Tenant.Name
+            }).ToList();
+
+            return Json(new
+            {
+                total = query.Count(),
+                rows = viewModel
+            });
         }
     }
 }
